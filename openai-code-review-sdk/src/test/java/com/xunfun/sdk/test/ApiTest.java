9e8072d2afe3dffa92319d91d46289048094118b
xufun <3463488701@qq.com>
2026-03-22 18:45:43 +0800

    工程初始化

---
+++ openai-code-review-sdk/src/test/java/com/xunfun/sdk/test/ApiTest.java
@@ -0,0 +1,7 @@
+package com.xunfun.sdk.test;
+
+public class ApiTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ openai-code-review-test/src/main/java/com/xufun/Application.java
@@ -0,0 +1,7 @@
+package com.xufun;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
