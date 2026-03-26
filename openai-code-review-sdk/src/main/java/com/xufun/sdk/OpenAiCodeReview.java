4e9ae0663f7d28e16d8ae38bea7afb05f7955319
xufun <3463488701@qq.com>
2026-03-26 16:28:49 +0800

    test15

--- openai-code-review-sdk/src/main/java/com/xufun/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/xufun/sdk/OpenAiCodeReview.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 public class OpenAiCodeReview {
     private static final Logger logger = LoggerFactory.getLogger(OpenAiCodeReview.class);
-//测试提交第15次
+//测试提交第16次
     public static void main(String[] args) throws Exception {
         GitCommand gitCommand = new GitCommand(
                 getEnv("GITHUB_REVIEW_LOG_URI"),
